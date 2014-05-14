37be9141cbcafb51ebef4ba76a5c2f1dcd9449d1
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-05-13 21:51:14 -0700

    Update example autograding code to use heading names

--- example/test1_autograder.py
+++ example/test1_autograder.py
@@ -1,24 +1,28 @@
 from nose.tools import eq_ as assert_eq
 
-@score(problem="hello", points=0.5)
+
+@score(problem="Problem 1/Part A", points=0.5)
 def grade_hello1():
     """Grade 'hello' with input 'Jessica'"""
     msg = hello("Jessica")
     assert_eq(msg, "Hello, Jessica!")
 
-@score(problem="hello", points=0.5)
+
+@score(problem="Problem 1/Part A", points=0.5)
 def grade_hello2():
     """Grade 'hello' with input 'Python'"""
     msg = hello("Python")
     assert_eq(msg, "Hello, Python!")
 
-@score(problem="goodbye", points=0.5)
+
+@score(problem="Problem 1/Part B", points=0.5)
 def grade_goodbye1():
     """Grade 'goodbye' with input 'Jessica'"""
     msg = goodbye("Jessica")
     assert_eq(msg, "Goodbye, Jessica")
 
-@score(problem="goodbye", points=0.5)
+
+@score(problem="Problem 1/Part B", points=0.5)
 def grade_goodbye2():
     """Grade 'goodbye' with input 'Python'"""
     msg = goodbye("Python")
