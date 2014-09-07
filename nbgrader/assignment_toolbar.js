c55746f1de35bcf6f00ef563ebf2824ee2a8a9f9
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-09-07 15:58:00 -0700

    Fix display of points on load/cell type change

--- nbgrader/assignment_toolbar.js
+++ nbgrader/assignment_toolbar.js
@@ -12,22 +12,44 @@
 **/
 
 define([
+    'base/js/namespace',
     'jquery',
     'notebook/js/celltoolbar',
 
-], function ($, celltoolbar) {
+], function (IPython, $, celltoolbar) {
     "use strict";
 
     var CellToolbar = celltoolbar.CellToolbar,
 
-        handleCellType = function (cell, cell_type) {
-            if (cell_type === "grade") {
-                cell.celltoolbar.inner_element.find(".assignment-points").show();
-                cell.element.addClass("assignment-gradeable-cell");
+        handleCellType = function (cell, points_elem) {
+            var cls = "assignment-gradeable-cell",
+                elem = cell.element,
+                cell_type;
+
+            if (cell.metadata.assignment === undefined) {
+                cell_type = "-";
             } else {
-                cell.celltoolbar.inner_element.find(".assignment-points").hide();
-                cell.element.removeClass("assignment-gradeable-cell");
+                cell_type = cell.metadata.assignment.cell_type;
             }
+
+            if (!points_elem) {
+                points_elem = elem.find(".assignment-points");
+            }
+
+            if (elem && cell_type === "grade") {
+                points_elem.show();
+                if (elem && !elem.hasClass(cls)) {
+                    elem.addClass(cls);
+                }
+
+            } else if (elem) {
+                points_elem.hide();
+                if (elem && elem.hasClass(cls)) {
+                    elem.removeClass(cls);
+                }
+            }
+
+            return cell_type;
         },
 
         select_type = CellToolbar.utils.select_ui_generator(
@@ -44,45 +66,46 @@ define([
                     cell.metadata.assignment = {};
                 }
                 cell.metadata.assignment.cell_type = value;
-                handleCellType(cell, value);
+                handleCellType(cell);
             },
 
-            function (cell) {
-                var cell_type;
-                if (cell.metadata.assignment === undefined) {
-                    cell_type = "-";
-                } else {
-                    cell_type = cell.metadata.assignment.cell_type;
-                }
-                handleCellType(cell, cell_type);
-                return cell_type;
-            }
+            handleCellType
         ),
 
+        getPoints = function (cell) {
+            if (cell.metadata.assignment === undefined) {
+                return undefined;
+            } else {
+                return cell.metadata.assignment.points;
+            }
+        },
+
+        setPoints = function (cell, value) {
+            if (cell.metadata.assignment === undefined) {
+                cell.metadata.assignment = {};
+            }
+            cell.metadata.assignment.points = value;
+            console.log("points: " + value);
+        },
+
         points = function (div, cell, celltoolbar) {
-            var f = CellToolbar.utils.input_ui_generator(
-                "Points: ",
-                function (cell, value) {
-                    if (cell.metadata.assignment === undefined) {
-                        cell.metadata.assignment = {};
-                    }
-                    cell.metadata.assignment.points = value;
-                    console.log("points: " + value);
-                },
+            var button_container = $(div);
+            button_container.addClass("assignment-points");
 
-                function (cell) {
-                    if (cell.metadata.assignment === undefined) {
-                        return undefined;
-                    } else {
-                        return cell.metadata.assignment.points;
-                    }
-                },
+            var text = $('<input/>').attr('type', 'text');
+            var lbl = $('<label/>').append($('<span/>').text("Points: "));
+            lbl.append(text);
 
-                "3em"
-            );
+            text.addClass("assignment-points-input");
+            text.attr("value", getPoints(cell));
+            text.keyup(function(){
+                setPoints(cell, text.val());
+            });
 
-            $(div).addClass("assignment-points");
-            f(div, cell, celltoolbar);
+            button_container.append($('<span/>').append(lbl));
+            IPython.keyboard_manager.register_events(text);
+
+            handleCellType(cell, button_container);
         },
 
         load_css = function () {
@@ -99,7 +122,7 @@ define([
             CellToolbar.register_callback('create_assignment.select', select_type);
             CellToolbar.register_callback('create_assignment.points', points);
 
-            var create_preset = ['create_assignment.points', 'create_assignment.select'];
+            var create_preset = ['create_assignment.select', 'create_assignment.points'];
             CellToolbar.register_preset('Create Assignment', create_preset, notebook);
             console.log('Assignment extension for metadata editing loaded.');
         };
