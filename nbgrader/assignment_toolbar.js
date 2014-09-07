451dbab0f63bca98d790dca5986210a7723e2360
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-09-07 13:55:46 -0700

    Style the assignment toolbar better

--- nbgrader/assignment_toolbar.js
+++ nbgrader/assignment_toolbar.js
@@ -1,4 +1,4 @@
-/*global define */
+/*global define, require */
 
 define([
     'jquery',
@@ -22,43 +22,71 @@ define([
                     cell.metadata.assignment = {};
                 }
                 cell.metadata.assignment.cell_type = value;
+                if (value === "grade") {
+                    cell.celltoolbar.inner_element.find(".assignment-points").show();
+                } else {
+                    cell.celltoolbar.inner_element.find(".assignment-points").hide();
+                }
             },
 
             function (cell) {
+                var cell_type;
                 if (cell.metadata.assignment === undefined) {
-                    return "-";
+                    cell_type = "-";
                 } else {
-                    return cell.metadata.assignment.cell_type;
+                    cell_type = cell.metadata.assignment.cell_type;
                 }
+                if (cell_type === "grade") {
+                    cell.celltoolbar.inner_element.find(".assignment-points").show();
+                } else {
+                    cell.celltoolbar.inner_element.find(".assignment-points").hide();
+                }
+                return cell_type;
             }
         ),
 
-        points = CellToolbar.utils.input_ui_generator(
-            "Points: ",
-            function (cell, value) {
-                if (cell.metadata.assignment === undefined) {
-                    cell.metadata.assignment = {};
-                }
-                cell.metadata.assignment.points = value;
-                console.log("points: " + value);
-            },
+        points = function (div, cell, celltoolbar) {
+            var f = CellToolbar.utils.input_ui_generator(
+                "Points: ",
+                function (cell, value) {
+                    if (cell.metadata.assignment === undefined) {
+                        cell.metadata.assignment = {};
+                    }
+                    cell.metadata.assignment.points = value;
+                    console.log("points: " + value);
+                },
 
-            function (cell) {
-                if (cell.metadata.assignment === undefined) {
-                    return undefined;
-                } else {
-                    return cell.metadata.assignment.points;
-                }
-            },
+                function (cell) {
+                    if (cell.metadata.assignment === undefined) {
+                        return undefined;
+                    } else {
+                        return cell.metadata.assignment.points;
+                    }
+                },
 
-            "3em"
-        ),
+                "3em"
+            );
+
+            $(div).addClass("assignment-points");
+            f(div, cell, celltoolbar);
+        },
+
+        load_css = function () {
+            var link = document.createElement("link");
+            link.type = "text/css";
+            link.rel = "stylesheet";
+            link.href = "/nbextensions/assignment.css";
+            console.log(link);
+            document.getElementsByTagName("head")[0].appendChild(link);
+        },
 
         load_ipython_extension = function (notebook) {
+            load_css();
+
             CellToolbar.register_callback('create_assignment.select', select_type);
             CellToolbar.register_callback('create_assignment.points', points);
 
-            var create_preset = ['create_assignment.select', 'create_assignment.points'];
+            var create_preset = ['create_assignment.points', 'create_assignment.select'];
             CellToolbar.register_preset('Create Assignment', create_preset, notebook);
 
             console.log('Assignment extension for metadata editing loaded.');
@@ -66,4 +94,5 @@ define([
 
     return {'load_ipython_extension': load_ipython_extension};
 });
+
 
