7aea2022839c87bafd2a6405f7c4fbdba4d8eb4c
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-09-07 19:26:17 -0700

    Fix #11

--- nbgrader/assignment_toolbar.js
+++ nbgrader/assignment_toolbar.js
@@ -21,35 +21,39 @@ define([
 
     var CellToolbar = celltoolbar.CellToolbar,
 
-        handleCellType = function (cell, points_elem) {
-            var cls = "assignment-gradeable-cell",
-                elem = cell.element,
-                cell_type;
-
+        getCellType = function (cell) {
             if (cell.metadata.assignment === undefined) {
-                cell_type = "-";
+                return "-";
             } else {
-                cell_type = cell.metadata.assignment.cell_type;
+                return cell.metadata.assignment.cell_type;
             }
+        },
 
-            if (elem && !points_elem) {
-                points_elem = elem.find(".assignment-points");
+        updateGradeable = function (cell, cell_type) {
+            var cls = "assignment-gradeable-cell",
+                elem = cell.element;
+
+            if (elem && cell_type === "grade" && !elem.hasClass(cls)) {
+                elem.addClass(cls);
+            } else if (elem && cell_type !== "grade" && elem.hasClass(cls)) {
+                elem.removeClass(cls);
             }
+        },
 
-            if (elem && cell_type === "grade") {
-                points_elem.show();
-                if (elem && !elem.hasClass(cls)) {
-                    elem.addClass(cls);
+        updatePoints = function (cell, cell_type, points_elem) {
+            var elem = cell.element;
+
+            if (elem) {
+                if (!points_elem) {
+                    points_elem = elem.find(".assignment-points");
                 }
 
-            } else if (elem) {
-                points_elem.hide();
-                if (elem && elem.hasClass(cls)) {
-                    elem.removeClass(cls);
+                if (cell_type === "grade") {
+                    points_elem.show();
+                } else {
+                    points_elem.hide();
                 }
             }
-
-            return cell_type;
         },
 
         select_type = CellToolbar.utils.select_ui_generator(
@@ -66,10 +70,17 @@ define([
                     cell.metadata.assignment = {};
                 }
                 cell.metadata.assignment.cell_type = value;
-                handleCellType(cell);
+                var cell_type = getCellType(cell);
+                updateGradeable(cell, cell_type);
+                updatePoints(cell, cell_type);
             },
 
-            handleCellType
+            function (cell) {
+                var cell_type = getCellType(cell);
+                updateGradeable(cell, cell_type);
+                updatePoints(cell, cell_type);
+                return cell_type;
+            }
         ),
 
         getPoints = function (cell) {
@@ -85,7 +96,6 @@ define([
                 cell.metadata.assignment = {};
             }
             cell.metadata.assignment.points = value;
-            console.log("points: " + value);
         },
 
         points = function (div, cell, celltoolbar) {
@@ -105,7 +115,9 @@ define([
             button_container.append($('<span/>').append(lbl));
             IPython.keyboard_manager.register_events(text);
 
-            handleCellType(cell, button_container);
+            var cell_type = getCellType(cell);
+            updateGradeable(cell, cell_type);
+            updatePoints(cell, cell_type, button_container);
         },
 
         load_css = function () {
@@ -125,6 +137,12 @@ define([
             var create_preset = ['create_assignment.select', 'create_assignment.points'];
             CellToolbar.register_preset('Create Assignment', create_preset, notebook);
             console.log('Assignment extension for metadata editing loaded.');
+
+            // update css class for whether cells are gradeable
+            var cells = IPython.notebook.get_cells();
+            for (var i = 0; i < cells.length; i++) {
+                updateGradeable(cells[i], getCellType(cells[i]));
+            }
         };
 
     return {'register': register};
