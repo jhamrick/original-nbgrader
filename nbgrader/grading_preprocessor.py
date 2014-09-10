f9bfef0fd2e366bd118d54dc9379e768a4001dc3
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-09-09 20:42:49 -0700

    Print out scores in grading preprocessor

--- nbgrader/grading_preprocessor.py
+++ nbgrader/grading_preprocessor.py
@@ -1,8 +1,8 @@
 import json
-import numpy as np
 
 from IPython.nbconvert.preprocessors import ExecutePreprocessor
 from IPython.utils.traitlets import Unicode
+from IPython.nbformat.current import new_output
 
 
 class Grader(ExecutePreprocessor):
@@ -11,8 +11,6 @@ class Grader(ExecutePreprocessor):
     autograder_tests_file = Unicode(
         "", config=True,
         info_text="Path to JSON file containing autograding code")
-    scores_file = Unicode(
-        "", config=True, info_text="Path to save scores")
 
     def preprocess(self, nb, resources):
         if len(nb.worksheets) != 1:
@@ -32,24 +30,6 @@ class Grader(ExecutePreprocessor):
         # execute all the cells
         nb, resources = super(Grader, self).preprocess(nb, resources)
 
-        scores = {}
-        for cell in nb.worksheets[0].cells:
-            if 'assignment' not in cell.metadata:
-                continue
-            meta = cell.metadata['assignment']
-            assignment_cell_type = meta['cell_type']
-            if assignment_cell_type == "grade" and 'tests' in meta:
-                passed = np.array(
-                    [resources['tests'][x] for x in meta['tests']])
-                weights = np.ones(len(passed)) / len(passed)
-                total = float(meta['points'])
-                scores[meta['id']] = total * np.sum(passed * weights)
-
-        # save the scores to file
-        with open(self.scores_file, 'w') as fh:
-            json.dump(scores, fh)
-        self.log.info("Saving scores to '{}'".format(self.scores_file))
-
         return nb, resources
 
     def preprocess_cell(self, cell, resources, cell_index):
@@ -60,22 +40,27 @@ class Grader(ExecutePreprocessor):
             test_id = meta.get('id', '')
             if test_id in self.autograder_tests:
                 if cell.cell_type == 'code':
-                    cell.input = self.autograder_tests[test_id]
+                    cell.input = self.autograder_tests[test_id]['source']
                 else:
-                    cell.source = self.autograder_tests[test_id]
+                    cell.source = self.autograder_tests[test_id]['source']
 
         cell, resources = super(Grader, self).preprocess_cell(
             cell, resources, cell_index)
 
         if assignment_cell_type == "autograder":
-            if 'tests' not in resources:
-                resources['tests'] = {}
+            if 'test_scores' not in resources:
+                resources['test_scores'] = {}
 
             errors = [x for x in cell.outputs if x['output_type'] == 'pyerr']
-            if len(errors) > 0:
-                score = 0
-            else:
-                score = 1
-            resources['tests'][meta['id']] = score
+            passed = int(len(errors) == 0)
+            total_score = self.autograder_tests[test_id]['points']
+            score = passed * total_score
+
+            output = new_output(
+                'stream',
+                output_text='Score: {} / {}'.format(score, total_score))
+            cell.outputs.insert(0, output)
+
+            resources['test_scores'][meta['id']] = score
 
         return cell, resources
